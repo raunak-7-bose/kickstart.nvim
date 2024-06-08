78540c13d33b4e3bcdcb997c882cba86deb9425e
Chrisemecello <158995146+raunak-7-bose@users.noreply.github.com>
2024-06-08 15:50:06 +0800

    added some tweaks to lua line, made it more round

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -15,6 +15,16 @@ return {
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
       },
+
+      sections = {
+
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 1 } },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { { 'location', separator = { right = '' }, left_padding = 1 } },
+      },
     }
   end,
 }
