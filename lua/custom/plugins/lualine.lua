5696d88bde1737837b23bab17f9848f417864f9f
raunak-macM1 <raunak-7-bose@users.noreply.github.com>
2024-11-12 13:04:04 -0600

    Made some changes to the seperators in lua line, might continue to change them in future.

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -12,18 +12,18 @@ return {
 
         theme = 'auto',
 
-        section_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
       },
 
       sections = {
 
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+        lualine_a = { { 'mode', separator = { left = '', right = '' }, right_padding = 2 } },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
-        lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
+        lualine_z = { { 'location', separator = { right = '', left = '' }, left_padding = 2 } },
       },
     }
   end,
